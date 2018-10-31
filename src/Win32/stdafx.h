d92f6967cb50a1a162ac4496766512bd55c6934e
Matthew Polsom <34988251+MattMan569@users.noreply.github.com>
2018-10-31 14:09:25 -0600

    Moved to Win32 subfolder

---
+++ src/Win32/stdafx.h
@@ -0,0 +1,29 @@
+﻿// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+// Exclude rarely-used stuff from Windows headers
+#define WIN32_LEAN_AND_MEAN     
+
+// Windows Header Files:
+#include <Windows.h>
+
+// C RunTime Header Files
+#include <malloc.h>
+#include <memory.h>
+#include <stdlib.h>
+#include <tchar.h>
+
+
+
+
+// Additional Header Files
+#include <fcntl.h>
+#include <io.h>
+#include <iostream>
+#include <string>
